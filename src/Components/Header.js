40a65231ccdadc0dfb20ad09964d0366b5d14be7
Kalum Wildig <kalumwildig@Kalums-MacBook-Pro.local>
2022-03-11 14:30:57 +0000

    h2 added" "

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -16,7 +16,7 @@ const Header = () => {
       <nav className="navbar">
         <div className="navbar__container">
           <Link to="/" id="navbar__logo">
-           <span> KW News </span>
+           <span> <h2>KW News</h2> </span>
           </Link>
           <div
             onClick={mobileMenu}
